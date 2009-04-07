95b1886c02e6c112d3263db16dcb513771b7f085
dk <dk@7b8d6398-3ce1-420b-bb7c-ec6fe894fba9>
2009-04-07 02:50:54 +0000

    changing name space
    git-svn-id: svn://viy/dmytro/Development/perl/modules/DBIx-Report-Excel/trunk@2494 7b8d6398-3ce1-420b-bb7c-ec6fe894fba9

--- example.pl
+++ example.pl
@@ -11,8 +11,10 @@ my $report = DBIx::Report::Excel->new( "SQLite.xls" );
 
 $report->dbh(DBI->connect("dbi:SQLite:dbname=/tmp/testdb","",""));
 
-# First worksheet has name People Names and defined column aliases
-# "First Name" and "Family Name".
+
+				# First worksheet has name People
+				# Names and defined column aliases
+				# "First Name" and "Family Name".
 $report->sql(
     qq
     {
@@ -23,19 +25,25 @@ SELECT first_name as "First Name", last_name as "Family name" FROM people
     });
 $report->write();
 
-# Examples below have optional SQL parameter for write() method.
+				# Examples below have optional SQL
+                                # parameter for write() method.
 
-# Generic worksheet name Sheet2 and column names f_name, color
-# obtained from parsing SQL.
+				# Generic worksheet name Sheet2 and
+                                # column names f_name, color obtained
+                                # from parsing SQL.
 $report->write("SELECT f_name, color from fruits");
 
-# Generic worksheet name Sheet3 and generic column names Column1,
-# Column2.
+				# Generic worksheet name Sheet3 and
+				# generic column names Column1,
+				# Column2.
 $report->write("SELECT * from fruits");
 
-# Two SQL statements in one write() call. Both SQL's define worksheet
-# name as YAML structure data. 1st SQL - column names f_name,
-# color. @nd SQL: generic column names ColumnX.
+
+				# Two SQL statements in one write()
+				# call. Both SQL's define worksheet
+				# name as YAML structure data. 1st SQL
+				# - column names f_name, color. @nd
+				# SQL: generic column names ColumnX.
 $report->write(
     qq 
     {
--- t/DBIx-Report-Excel.t
+++ t/DBIx-Report-Excel.t
@@ -6,7 +6,7 @@
 # change 'tests => 1' to 'tests => last_test_to_print';
 
 use Test::More tests => 1;
-BEGIN { use_ok('SQL::Reporter::Excel') };
+BEGIN { use_ok('DBIx::Report::Excel') };
 
 #########################
 
